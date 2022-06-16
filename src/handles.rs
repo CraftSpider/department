3b82bd13a62dfa885dcdfaaac637cb0f6a528ab5
Rune Tynan <runetynan@gmail.com>
2022-06-16 18:50:56 -0400

    Add note that users can still implement `ThinBox` themselves

--- src/handles.rs
+++ src/handles.rs
@@ -14,9 +14,10 @@ use core::ptr::{NonNull, Pointee};
 /// The fact that this supplies casting and metadata-retrieval slightly limits handles - they must
 /// either be able to dereference themselves, or must hold the metadata for a type inline. On the
 /// other hand, even our most restrictive storage does it this way, and implementing always-thin
-/// handles, while theoretically possible, is deemed not worth the limitations. This decision is
-/// open to change before the release of 1.0 - feel free to open an issue if you have a compelling
-/// use-case.
+/// handles, while theoretically possible, is deemed not worth the limitations. It is also possible
+/// to implement such always-thin items via a custom `ThinBox` style type, meaning the lack of them
+/// in this API does not prevent storages from having them entirely. This decision is open to change
+/// before the release of 1.0 - feel free to open an issue if you have a compelling use-case.
 pub trait Handle<T: ?Sized + Pointee> {
     /// The type of address for this handle. This is only [`PartialOrd`] instead of [`Ord`] because
     /// some handles may not be strictly greater or lesser than others (See `FallbackStorage`)
